50c26a49193d631c71ce0ef220dac37d5caf6ceb
Shivam Dattatray Shinde <shivamshinde92722@gmail.com>
2022-01-02 23:54:45 +0530

    added some code and comments in report_creation function

--- eda_methods.py
+++ eda_methods.py
@@ -12,6 +12,12 @@ class EDA:
         self.data = pd.read_csv(url)
 
     def report_creation(self):
-        my_report = sv.analyze(self.data)
+        ## Sweetviz library only supports numerical dependent feature
+        ## That's why, when the dependent feature is numerical, we will provide it as target feature in analyze function
+        ## else, we won't supply it
+        if self.data[self.data.columns[-1]].dtypes != 'O':
+            my_report = sv.analyze(self.data,self.data.columns[-1])
+        else:
+            my_report = sv.analyze(self.data)
         my_report.show_html('templates/results.html',open_browser=False)
 
