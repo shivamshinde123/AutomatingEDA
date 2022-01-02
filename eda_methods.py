8a52fee11317baf6b79e6146f7a5621fff58f701
Shivam Dattatray Shinde <shivamshinde92722@gmail.com>
2022-01-02 12:47:22 +0530

    added some more basic inspection methods

--- eda_methods.py
+++ eda_methods.py
@@ -16,17 +16,34 @@ class EDA:
 
     ## Creating methods to get the basic info of entered data file
     def shape_of_dataset(self):
-        return self.data.shape
+        print(f"The shape of the dataset is {self.data.shape}/n")
 
     def no_of_rows(self):
-        return self.data.shape[0]
+        print(f"Dataset has {self.data.shape[0]} rows/n")
 
     def no_of_columns(self):
-        return self.data.shape[1]
+        print(f"Dataset has {self.data.shape[1]} columns/n")
 
     def dataset_info(self):
+        print("Dataset Info:")
         return self.data.info()
 
     def dataset_description(self):
+        print("/n")
         return self.data.describe()
+
+    def uniques_values(self):
+        print('/n')
+        for feature in self.data.columns:
+            print(f"The feature {feature} has {self.data[feature].nunique()} unique values")
+        print('/n')
+    def feature_types(self):
+        numerical_features = [feature for feature in self.data.columns if self.data[feature].dtypes != 'O']
+        categorical_features = [feature for feature in self.data.columns if feature not in numerical_features]
+        numerical_discrete_features = [feature for feature in numerical_features if self.data[feature].nunique() < 10]
+        numerical_continuous_features = [feature for feature in numerical_features if feature not in numerical_discrete_features]
+
+        print(f"Numerical discrete features in the dataset are: {numerical_discrete_features}")
+        print(f"Numerical continuous features in the dataset are: {numerical_continuous_features}")
+        print(f"Categorical features in the dataset are: {categorical_features}")
 
