74e1b600cf53b73d6c172524d5f7ea35c184dd31
Shivam Dattatray Shinde <shivamshinde92722@gmail.com>
2022-01-02 13:38:02 +0530

    added route functions in app.py file

--- app.py
+++ app.py
@@ -1,4 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from eda_methods import EDA
 
 app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/results',methods=['POST'])
+def results():
+    data = request.form['dataset']
+    obj = EDA(data)
+    return render_template('results.html',obj=obj)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
